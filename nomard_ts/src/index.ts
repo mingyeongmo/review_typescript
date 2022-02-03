b7530d977fb8327c615c5cb3ac6295c5a74a4abb
mingyeongmo <mingyeongmp@naver.com>
2022-02-03 16:53:32 +0900

    Interfaces on ts

--- nomard_ts/src/index.ts
+++ nomard_ts/src/index.ts
@@ -1,13 +1,25 @@
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "minmoci",
+  age: 19,
+  gender: "male",
+};
+
 const name = "minmo",
   age = 19,
   gender = "male";
 // const age = 19;
 // const gender = "male";
 
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are age ${age}, you are a ${gender}!`;
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are age ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(person));
 
 export {};
