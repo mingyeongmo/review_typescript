b84745eea63ceac231aa7e30358c0bde0c00b752
mingyeongmo <mingyeongmp@naver.com>
2022-02-05 17:07:21 +0900

    Generics interface,type,class

--- react_ts/dist/practice.js
+++ react_ts/dist/practice.js
@@ -1,19 +1,32 @@
 "use strict";
-// function merge(a: any, b: any): any {
-//   return {
-//     ...a,
-//     ...b,
-//   };
-// }
-// const merged = merge({ foo: 1 }, { bar: 1 });
-function merge(a, b) {
-    return Object.assign(Object.assign({}, a), b);
+const items = {
+    list: ["a", "b", "c"],
+};
+const itemss = {
+    list: [10, 9, 8],
+};
+class Queue {
+    constructor() {
+        this.list = [];
+    }
+    get length() {
+        return this.list.length;
+    }
+    enqueue(item) {
+        this.list.push(item);
+    }
+    dequeue() {
+        return this.list.shift();
+    }
 }
-const merged = merge({ foo: 1 }, { bar: 1 });
-function wrap(param) {
-    return {
-        param,
-    };
-}
-const wrapped = wrap(10);
-console.log(wrap);
+const queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
