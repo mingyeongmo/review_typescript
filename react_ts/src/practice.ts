a0ff695295169cbfb0e13a3547123d739ff64763
mingyeongmo <mingyeongmp@naver.com>
2022-02-05 16:26:07 +0900

    public priavate extends

---
+++ react_ts/src/practice.ts
@@ -0,0 +1,62 @@
+// Shape 라는 interface 를 선언한다.
+interface Shape {
+  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
+}
+
+class Circle implements Shape {
+  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+
+  constructor(public radius: number) {
+    this.radius = radius;
+  }
+
+  // 너비를 가져오는 함수를 구현합니다.
+  getArea(): number {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
+
+interface Person {
+  name: string;
+  age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+}
+// interface Developer {
+//   name: string;
+//   age?: number;
+//   skills: string[];
+// }
+
+interface Developer extends Person {
+  skills: string[];
+}
+
+const person: Person = {
+  name: "감사림",
+  age: 20,
+};
+
+const expert: Developer = {
+  name: "김개발",
+  skills: ["javascript", "react"],
+};
