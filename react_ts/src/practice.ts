d649d30780c0bf93a5ef6518f69a7f278fcfd6c1
mingyeongmo <mingyeongmp@naver.com>
2022-02-05 16:32:01 +0900

    type

--- react_ts/src/practice.ts
+++ react_ts/src/practice.ts
@@ -37,17 +37,12 @@ shapes.forEach((shape) => {
   console.log(shape.getArea());
 });
 
-interface Person {
+type Person {
   name: string;
   age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
 }
-// interface Developer {
-//   name: string;
-//   age?: number;
-//   skills: string[];
-// }
 
-interface Developer extends Person {
+type Developer = Person & {
   skills: string[];
 }
 
@@ -60,3 +55,10 @@ const expert: Developer = {
   name: "김개발",
   skills: ["javascript", "react"],
 };
+
+type People = Person[]; // Person[]를 이제 앞으로 People 이라는 타입으로 사용 할 수 있습니다.
+const people: People = [person,expert];
+
+type Color = 'red' | 'orange' | 'yellow';
+const color: Color = 'red';
+const colors: Color[] = ['red', 'orange'];
