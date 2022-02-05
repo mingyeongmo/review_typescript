97a9d61b3bb7493d56cc519f4c40bd7fea4226bb
mingyeongmo <mingyeongmp@naver.com>
2022-02-05 16:40:58 +0900

    Generics 사용하기

--- react_ts/dist/practice.js
+++ react_ts/dist/practice.js
@@ -1,30 +1,19 @@
 "use strict";
-class Circle {
-    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
-    constructor(radius) {
-        this.radius = radius;
-        this.radius = radius;
-    }
-    // 너비를 가져오는 함수를 구현합니다.
-    getArea() {
-        return this.radius * this.radius * Math.PI;
-    }
+// function merge(a: any, b: any): any {
+//   return {
+//     ...a,
+//     ...b,
+//   };
+// }
+// const merged = merge({ foo: 1 }, { bar: 1 });
+function merge(a, b) {
+    return Object.assign(Object.assign({}, a), b);
 }
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.width = width;
-        this.height = height;
-    }
-    getArea() {
-        return this.width * this.height;
-    }
+const merged = merge({ foo: 1 }, { bar: 1 });
+function wrap(param) {
+    return {
+        param,
+    };
 }
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-console.log(circle.radius);
-const shapes = [new Circle(5), new Rectangle(10, 5)];
-shapes.forEach((shape) => {
-    console.log(shape.getArea());
-});
+const wrapped = wrap(10);
+console.log(wrap);
--- react_ts/src/practice.ts
+++ react_ts/src/practice.ts
@@ -1,64 +1,25 @@
-// Shape 라는 interface 를 선언한다.
-interface Shape {
-  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
+// function merge(a: any, b: any): any {
+//   return {
+//     ...a,
+//     ...b,
+//   };
+// }
+
+// const merged = merge({ foo: 1 }, { bar: 1 });
+
+function merge<A, B>(a: A, b: B): A & B {
+  return {
+    ...a,
+    ...b,
+  };
 }
 
-class Circle implements Shape {
-  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+const merged = merge({ foo: 1 }, { bar: 1 });
 
-  constructor(public radius: number) {
-    this.radius = radius;
-  }
-
-  // 너비를 가져오는 함수를 구현합니다.
-  getArea(): number {
-    return this.radius * this.radius * Math.PI;
-  }
+function wrap<T>(param: T) {
+  return {
+    param,
+  };
 }
 
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  getArea() {
-    return this.width * this.height;
-  }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach((shape) => {
-  console.log(shape.getArea());
-});
-
-type Person {
-  name: string;
-  age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
-}
-
-type Developer = Person & {
-  skills: string[];
-}
-
-const person: Person = {
-  name: "감사림",
-  age: 20,
-};
-
-const expert: Developer = {
-  name: "김개발",
-  skills: ["javascript", "react"],
-};
-
-type People = Person[]; // Person[]를 이제 앞으로 People 이라는 타입으로 사용 할 수 있습니다.
-const people: People = [person,expert];
-
-type Color = 'red' | 'orange' | 'yellow';
-const color: Color = 'red';
-const colors: Color[] = ['red', 'orange'];
+const wrapped = wrap(10);
