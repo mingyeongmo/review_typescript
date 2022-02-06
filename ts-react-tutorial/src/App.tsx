db431b5f1803c2e394d1a842fa784ef1fc62e403
mingyeongmo <mingyeongmp@naver.com>
2022-02-06 15:43:56 +0900

    useState는 사실 generics를 사용하지 않아도 알아서 타입유추가 된다

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Greetings from "./Greetings";
+import Counter from "./Counter";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-
-  return (
-    <div className="App">
-      <Greetings name="MinMo" onClick={onClick} />
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
