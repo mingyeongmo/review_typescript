4d6c18375c16842860a15aac708dd88de88a37a7
mingyeongmo <mingyeongmp@naver.com>
2022-02-06 16:05:01 +0900

    인풋 상태관리하기

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 const App: React.FC = () => {
-  return <Counter />;
+  const onSubmit = (forms: { name: string; description: string }) => {
+    console.log(forms);
+  };
+
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
